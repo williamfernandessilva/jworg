3ed9df02308dd33a7afc3911473877d89fe6ca92
williamfernandessilva <willgsvs@hotmail.com>
2026-02-23 23:44:59 -0300

    teste

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Olá, Mundo!</h1>
+      <p>Meu projeto React + Vite no Windows está rodando!</p>
+    </div>
+  )
+}
+
+export default App    
