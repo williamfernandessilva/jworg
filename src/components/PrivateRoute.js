ea7f6b255794e47ac2bf80e91f243a5f8d608205
williamfernandessilva <willgsvs@hotmail.com>
2026-03-24 23:02:49 -0300

    proteçãooaaaa

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ children }) => {
+export function PrivateRoute({ children }) {
   const usuarioLogado = localStorage.getItem("nomeUsuario");
 
-  // Se não houver usuário, redireciona para o login
   if (!usuarioLogado) {
     return <Navigate to="/" />;
   }
 
-  // Se houver, renderiza os filhos (Mapa ou Tutorial)
   return children;
-};
-
-export { PrivateRoute };
+}
